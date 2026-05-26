9565066a4a54b683fe25d91571e920790982399e
Ihor Hurianov <igor.gurianov@gmail.com>
2026-05-26 15:42:42 +0300

    Remove hidden characters

--- task.sql
+++ task.sql
@@ -1,9 +1,9 @@
 USE ShopDB
-​
-​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT UPDATE, DELETE, SELECT, INSERT ON ShopDB.* TO 'webappuser'@'%';
 
-​​CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
 
 FLUSH PRIVILEGES;
